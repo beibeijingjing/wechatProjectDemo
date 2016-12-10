cad28b93dd87d2d6a06b26958fb59d665619694a
Administrator <Administrator@jsws>
2016-12-10 22:02:01 +0800

    框架优化

---
+++ src/main/java/core/utils/AccessTokenGetThread.java
@@ -0,0 +1,63 @@
+/** 
+ * @Prject: ssm-master
+ * @Package: weixin.server.utils 
+ * @Title: AccessTokenGetThread.java 
+ * Copyright © 2016Quanmingexing.All rights reserved.
+ * @author: 曹亚伟   
+ * @date: 2016年7月28日 下午3:46:57   
+ */
+package core.utils;
+
+import weixin.server.config.WxConfig;
+import weixin.server.entity.auth.WxAuth;
+import weixin.server.service.WxAuthService;
+import core.exception.WxBaseException;
+
+/**
+ * @ClassName: AccessTokenGetThread
+ * @Description: TODO
+ * @author: 曹亚伟
+ * @date: 2016年7月28日 下午3:46:57
+ */
+public class AccessTokenGetThread implements Runnable {
+
+	public static String appId = "";
+
+	public static String appSecret = "";
+
+	public static WxAuth wxAuth = null;
+
+	public static String accessToken = "";
+
+	@Override
+	public void run() {
+
+		while (true) {
+			try {
+				WxAuthService authService = new WxAuthService();
+				wxAuth = authService.getAccessToken(appId, appSecret);
+				if (wxAuth != null && wxAuth.getAccessToken() != null
+						&& !"".equals(wxAuth.getAccessToken())) {
+					accessToken = wxAuth.getAccessToken();
+					WxConfig.accessToken = accessToken;
+					System.out.println("=========================accessToken："
+							+ AccessTokenGetThread.accessToken
+							+ "===============================");
+					Thread.sleep(1000 * 60 * 30);
+				} else {
+					Thread.sleep(1000 * 60);
+				}
+			} catch (WxBaseException e) {
+				e.printStackTrace();
+				try { // 发生异常休眠1秒
+					Thread.sleep(1000 * 10);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
---
+++ src/main/java/core/utils/HttpRequest.java
@@ -0,0 +1,223 @@
+/** 
+ * @Prject: ssm-master
+ * @Package: weixin.server.utils 
+ * @Title: HttpRequest.java 
+ * Copyright © 2016Quanmingexing.All rights reserved.
+ * @author: 曹亚伟   
+ * @date: 2016年12月6日 下午3:29:51   
+ */
+package core.utils;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+
+import core.exception.WxBaseException;
+import weixin.server.config.WxConfig;
+import weixin.server.entity.auth.WxAuth;
+import weixin.server.service.WxAuthService;
+
+/**
+ * @ClassName: HttpRequest
+ * @Description: TODO
+ * @author: 曹亚伟
+ * @date: 2016年12月6日 下午3:29:51
+ */
+public class HttpRequest {
+
+	public static String postJson(String uri, String jsonParm) {
+		String result = "";
+
+		try {
+			WxAuthService authService = new WxAuthService();
+			WxAuth wxAuth = authService.getAccessToken(
+					ResourceUtils.getResource("wx_appid"),
+					ResourceUtils.getResource("wx_appsecret"));
+			if (wxAuth == null) {
+				return null;
+			}
+			String accessToken = wxAuth.getAccessToken();
+			String action = uri + "?access_token=" + accessToken;
+
+			URL url = new URL(action);
+			HttpURLConnection http = (HttpURLConnection) url.openConnection();
+
+			http.setRequestMethod("POST");
+			http.setRequestProperty("Content-Type",
+					"application/x-www-form-urlencoded");
+			http.setDoOutput(true);
+			http.setDoInput(true);
+			System.setProperty("sun.net.client.defaultConnectTimeout", "30000");// 连接超时3ss0秒
+			System.setProperty("sun.net.client.defaultReadTimeout", "30000"); // 读取超时30秒
+			http.connect();
+			// 发送传递参数
+			OutputStream os = http.getOutputStream();
+			os.write(jsonParm.getBytes("UTF-8"));// 传入参数
+			os.flush();
+			os.close();
+
+			// 读取返回数据
+			InputStream is = http.getInputStream();
+			int size = is.available();
+			byte[] jsonBytes = new byte[size];
+			is.read(jsonBytes);
+			result = new String(jsonBytes, "UTF-8");
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (WxBaseException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	/**
+	 * 向指定URL发送GET方法的请求
+	 * 
+	 * @param url
+	 *            发送请求的URL
+	 * @param param
+	 *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+	 * @return URL 所代表远程资源的响应结果
+	 */
+	public static String sendGet(String url, String param) {
+		String result = "";
+		BufferedReader in = null;
+		try {
+			String urlNameString = url + "?" + param;
+			URL realUrl = new URL(urlNameString);
+			// 打开和URL之间的连接
+			URLConnection connection = realUrl.openConnection();
+			// 设置通用的请求属性
+			connection.setRequestProperty("accept", "*/*");
+			connection.setRequestProperty("connection", "Keep-Alive");
+			connection.setRequestProperty("user-agent",
+					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			// 建立实际的连接
+			connection.connect();
+			// 获取所有响应头字段
+			Map<String, List<String>> map = connection.getHeaderFields();
+			// 遍历所有的响应头字段
+			for (String key : map.keySet()) {
+				System.out.println(key + "--->" + map.get(key));
+			}
+			// 定义 BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			System.out.println("发送GET请求出现异常！" + e);
+			e.printStackTrace();
+		}
+		// 使用finally块来关闭输入流
+		finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * 向指定 URL 发送POST方法的请求
+	 * 
+	 * @param url
+	 *            发送请求的 URL
+	 * @param param
+	 *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+	 * @return 所代表远程资源的响应结果
+	 */
+	public static String sendPost(String url, String param) {
+		PrintWriter out = null;
+		BufferedReader in = null;
+		String result = "";
+		try {
+			URL realUrl = new URL(url);
+			// 打开和URL之间的连接
+			URLConnection conn = realUrl.openConnection();
+			// 设置通用的请求属性
+			conn.setRequestProperty("accept", "*/*");
+			conn.setRequestProperty("connection", "Keep-Alive");
+			conn.setRequestProperty("user-agent",
+					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			// 发送POST请求必须设置如下两行
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			// 获取URLConnection对象对应的输出流
+			out = new PrintWriter(conn.getOutputStream());
+			// 发送请求参数
+			out.print(param);
+			// flush输出流的缓冲
+			out.flush();
+			// 定义BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(
+					new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			System.out.println("发送 POST 请求出现异常！" + e);
+			e.printStackTrace();
+		}
+		// 使用finally块来关闭输出流、输入流
+		finally {
+			try {
+				if (out != null) {
+					out.close();
+				}
+				if (in != null) {
+					in.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	@SuppressWarnings("deprecation")
+	public static String doPost(String url, String param) {
+		HttpClient hc = new HttpClient();
+		PostMethod pm = new PostMethod(url);
+		pm.getParams().setParameter(HttpMethodParams.HTTP_CONTENT_CHARSET,
+				"UTF-8");
+
+		InputStream in = new ByteArrayInputStream(param.toString().getBytes());
+		pm.setRequestBody(in);
+		String html = "";
+		try {
+			hc.executeMethod(pm);
+			html = pm.getResponseBodyAsString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		pm.releaseConnection();
+		return html;
+	}
+
+}
